73b7138db06224132e2b464ba3b50cb7c0b28d2d
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-17 14:42:59 +0900

    Create 2884_알람시계.java

---
+++ 2884_알람시계.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int hour = s.nextInt();
+        int min = s.nextInt();
+        
+	if(min<45){
+            min = (min-45)+60;
+            if(hour==0){
+                hour = 23;
+	    }else{
+	         --hour;
+	     }
+        }else{
+            min = min-45;
+	    if(hour==0){
+            --hour;
+      	    }
+	}
+        System.out.print(hour+" "+min);
+        
+        s.close();
+    }
+}
+/*시간
+분
+경우3
+	1. min>=45
+	2. min <45
+		1. hour=0*/
+

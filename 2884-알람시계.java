ef10494b34cff05fe8583a067d9a7d7afe718db6
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-19 10:56:37 +0900

    Update 2884-알람시계.java

--- 2884-알람시계.java
+++ 2884-알람시계.java
@@ -5,25 +5,15 @@ public class Main{
         int hour = s.nextInt();
         int min = s.nextInt();
         
-	    if(min<45){
-            min = (min-45)+60;
-                if(hour==0){
-                    hour = 23;
-	            }else{
-	             --hour;
-	             }
-        }else{
-            min = min-45;
-        }
-
-        System.out.print(hour+" "+min);
+        if(min<45){
+            min += 15;
+            if(hour==0)
+                hour=23;
+            else
+                --hour;
+        }else
+            min -= 45; 
+        System.out.print(hour+" "+ min);
         s.close();
     }
 }
-/*시간
-분
-경우3
-	1. min>=45
-	2. min <45
-		1. hour=0
-*/
